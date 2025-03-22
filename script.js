632d109dc4ce534f082722dbe0db023d0cfb795b
Harsh932006 <hpal7241@gmail.com>
2025-03-22 10:44:54 +0530

    Final Project

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-let para = document.getElementById("para");
-let btn = document.querySelector("button");
-let tasks = document.querySelector(".tasks");
+const taskInput = document.querySelector("#taskInput");
+const addTaskButton = document.querySelector("#addTask");
+const todoList = document.querySelector("#todo-list");
 
-btn.addEventListener("click", () => {
-  let tasksText = para.value.trim();
+addTaskButton.addEventListener("click",()=>{
+  const taskText = taskInput.value.trim();
 
-  if(tasksText!==""){
-    let newTask = document.createElement("p");
-    newTask.innerHTML = tasksText;
-    tasks.appendChild(newTask);
-    para.value = "";
+  if(taskText==""){
+    return alert("Please enter a task!");
   }
-});
+
+  const li = document.createElement("li");
+
+  const taskSpan = document.createElement("span");
+  taskSpan.textContent = taskText;
+
+  taskSpan.addEventListener("click",()=>{
+    taskSpan.classList.toggle("completed");
+  })
+
+  const deletebtn = document.createElement("button");
+  deletebtn.textContent = "Delete";
+  deletebtn.className = "button"
+  deletebtn.addEventListener("click",()=>{
+    todoList.removeChild(li);
+  })
+  li.appendChild(taskSpan);
+  li.appendChild(deletebtn);
+  todoList.appendChild(li);
+
+  taskInput.value = "";
+})
